03d66c988a8a4478f4fd1fe0099eb39a961de63a
Nick Bell <87626769+ntbell@users.noreply.github.com>
2022-01-12 16:08:37 -0600

    v

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,16 +5,7 @@ const app = require("./app");
 const knex = require("./db/connection");
 console.log("Pulled app and knex");
 
-knex.migrate
-  .latest()
-  .then(app.listen(PORT, listener))
-  .catch((error) => {
-    console.error(error);
-    console.log("error found, entered catch")
-    knex.destroy();
-  });
-
-console.log("Called knex.migrate");
+app.listen(PORT, listener)
 
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
